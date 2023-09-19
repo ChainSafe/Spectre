0e6a02bc915053658d802638bf1d3a47b032a4ad
Timofey Luin <timofey.luin@gmail.com>
2023-09-19 15:49:12 +0200

    fix prover crate

--- prover/src/args.rs
+++ prover/src/args.rs
@@ -6,16 +6,19 @@ use strum::EnumString;
 pub struct Options {
     #[command(subcommand)]
     pub proof: Proof,
+
+    #[clap(long, short, default_value = "mainnet")]
+    pub spec: Spec,
 }
 
 #[derive(Clone, clap::Subcommand)]
 pub enum Proof {
-    CommitteeUpdate(CommitteeUpdateArgs),
-    SyncStep(SyncStepArgs),
+    CommitteeUpdate(Args),
+    SyncStep(Args),
 }
 
 #[derive(Clone, clap::Args)]
-pub struct SyncStepArgs {
+pub struct Args {
     #[clap(long, short, default_value = "proof")]
     pub out: Out,
 
@@ -34,29 +37,11 @@ pub struct SyncStepArgs {
 
     #[clap(index = 1, help = "path to output", default_value = ".")]
     pub path_out: PathBuf,
+
+    #[clap(index = 1, help = "Beacon node URL", default_value = "http://localhost::5052")]
+    pub node_url: String,
 }
 
-#[derive(Clone, clap::Args)]
-pub struct CommitteeUpdateArgs {
-    #[clap(long, short, default_value = "proof")]
-    pub out: Out,
-
-    #[clap(
-        long,
-        short,
-        default_value = "./lightclient-circuits/config/committee_update.json"
-    )]
-    pub config_path: PathBuf,
-
-    #[clap(long, short, default_value = "./build")]
-    pub build_dir: PathBuf,
-
-    #[clap(long, short, default_value = "./sync_state.json")]
-    pub input_path: PathBuf,
-
-    #[clap(index = 1, help = "path to output", default_value = ".")]
-    pub path_out: PathBuf,
-}
 
 #[derive(Clone, Debug, PartialEq, EnumString)]
 pub enum Out {
@@ -68,4 +53,14 @@ pub enum Out {
     EvmVerifier,
     #[strum(serialize = "calldata")]
     Calldata,
+}
+
+#[derive(Clone, Debug, PartialEq, EnumString)]
+pub enum Spec {
+    #[strum(serialize = "minimal")]
+    Minimal,
+    #[strum(serialize = "testnet")]
+    Testnet,
+    #[strum(serialize = "mainnet")]
+    Mainnet,
 }
--- prover/src/main.rs
+++ prover/src/main.rs
@@ -1,18 +1,14 @@
-use std::{cell::RefCell, env::set_var, fs, path::Path};
+use std::future::Future;
 
-use args::{Options, Out, Proof};
+use args::{Args, Options, Out, Proof};
 use cli_batteries::version;
-use eth_types::Test;
 use halo2curves::bn256::Fr;
 use lightclient_circuits::{
     committee_update_circuit::CommitteeUpdateCircuit,
     sync_step_circuit::SyncStepCircuit,
-    util::{read_params, read_vkey, AppCircuitExt},
-    witness::SyncState,
-    FlexGateConfigParams,
+    util::{gen_srs, AppCircuit},
 };
-use snark_verifier::loader::halo2::halo2_ecc::halo2_base::gates::builder::GateThreadBuilder;
-use snark_verifier_sdk::evm::{gen_evm_proof_shplonk, gen_evm_verifier_shplonk, write_calldata};
+use preprocessor::{fetch_rotation_args, fetch_step_args};
 
 mod args;
 
@@ -21,137 +17,101 @@ fn main() {
 }
 
 async fn app(options: Options) -> eyre::Result<()> {
-    match options.proof {
-        Proof::CommitteeUpdate(args) => match args.out {
-            
-            Out::Proof => {
-                gen_evm_proof::<CommitteeUpdateCircuit<Test, Fr>>(
-                    &args.config_path,
-                    &args.build_dir,
-                    &args.input_path,
-                    Some(&args.path_out),
-                    None,
-                );
-            }
-            Out::Artifacts => setup_circuit::<CommitteeUpdateCircuit<Test, Fr>>(
-                &args.config_path,
-                &args.build_dir,
-            ),
-            Out::EvmVerifier => gen_evm_verifier::<CommitteeUpdateCircuit<Test, Fr>>(
-                &args.config_path,
-                &args.build_dir.join("committee_update.vkey"),
-                &args.path_out,
-            ),
-            Out::Calldata => gen_evm_calldata::<CommitteeUpdateCircuit<Test, Fr>>(
-                &args.config_path,
-                &args.build_dir,
-                &args.input_path,
-                &args.path_out,
-            ),
-        },
-        Proof::SyncStep(args) => match args.out {
-            Out::Proof => {
-                gen_evm_proof::<SyncStepCircuit<Test, Fr>>(
-                    &args.config_path,
-                    &args.build_dir,
-                    &args.input_path,
-                    Some(&args.path_out),
-                    None,
-                );
-            }
-            Out::Artifacts => {
-                setup_circuit::<SyncStepCircuit<Test, Fr>>(&args.config_path, &args.build_dir)
-            }
-            Out::EvmVerifier => gen_evm_verifier::<SyncStepCircuit<Test, Fr>>(
-                &args.config_path,
-                &args.build_dir.join("sync_step.vkey"),
-                &args.path_out,
-            ),
-            Out::Calldata => gen_evm_calldata::<SyncStepCircuit<Test, Fr>>(
-                &args.config_path,
-                &args.build_dir,
-                &args.input_path,
-                &args.path_out,
-            ),
-        },
+    match options.spec {
+        args::Spec::Minimal => spec_app::<eth_types::Minimal>(&options.proof).await,
+        args::Spec::Testnet => spec_app::<eth_types::Testnet>(&options.proof).await,
+        args::Spec::Mainnet => spec_app::<eth_types::Testnet>(&options.proof).await,
+    }
+}
+
+async fn spec_app<S: eth_types::Spec>(proof: &Proof) -> eyre::Result<()> {
+    match proof {
+        Proof::CommitteeUpdate(args) => {
+            generic_circuit_app::<CommitteeUpdateCircuit<S, Fr>, _, _>(
+                args,
+                fetch_rotation_args,
+                "committee_update",
+            )
+            .await
+        }
+        Proof::SyncStep(args) => {
+            generic_circuit_app::<SyncStepCircuit<S, Fr>, _, _>(
+                args,
+                fetch_step_args,
+                "sync_step",
+            )
+            .await
+        }
+    }
+}
+
+async fn generic_circuit_app<
+    Circuit: AppCircuit,
+    FnFetch: FnOnce(String) -> Fut,
+    Fut: Future<Output = eyre::Result<Circuit::Args>>,
+>(
+    cli_args: &Args,
+    fetch: FnFetch,
+    name: &str,
+) -> eyre::Result<()> {
+    let k = Circuit::get_degree(&cli_args.config_path);
+    let params = gen_srs(k);
+    let pk_filename = format!("{}.pkey", name);
+
+    match cli_args.out {
+        Out::Proof => {
+            let pk = Circuit::read_or_create_pk(
+                &params,
+                cli_args.build_dir.join(&pk_filename),
+                &cli_args.config_path,
+                true,
+            );
+            let witness = fetch(cli_args.node_url.clone()).await?;
+            Circuit::gen_snark_shplonk(
+                &params,
+                &pk,
+                &cli_args.config_path,
+                Some(&cli_args.path_out),
+                &witness,
+            )
+            .map_err(|e| eyre::eyre!("Failed to generate proof: {}", e))?;
+        }
+        Out::Artifacts => {
+            Circuit::create_pk(
+                &params,
+                cli_args.build_dir.join(&pk_filename),
+                &cli_args.config_path,
+            );
+        }
+        Out::EvmVerifier => {
+            let pk = Circuit::read_or_create_pk(
+                &params,
+                cli_args.build_dir.join(&pk_filename),
+                &cli_args.config_path,
+                true,
+            );
+            Circuit::gen_evm_verifier_shplonk(&params, &pk, &cli_args.path_out)
+                .map_err(|e| eyre::eyre!("Failed to EVM verifier: {}", e))?;
+        }
+        Out::Calldata => {
+            let pk = Circuit::read_or_create_pk(
+                &params,
+                cli_args.build_dir.join(&pk_filename),
+                &cli_args.config_path,
+                true,
+            );
+            let witness = fetch(cli_args.node_url.clone()).await?;
+
+            Circuit::gen_calldata(
+                &params,
+                &pk,
+                &cli_args.config_path,
+                &cli_args.path_out,
+                None,
+                &witness,
+            )
+            .map_err(|e| eyre::eyre!("Failed to generate calldata: {}", e))?;
+        }
     }
     Ok(())
-}
-
-fn setup_circuit<C: AppCircuitExt<Fr>>(config_path: &Path, build_dir: &Path) {
-    let config: FlexGateConfigParams =
-        serde_json::from_slice(&fs::read(config_path).unwrap()).unwrap();
-
-    let _ = C::setup(config.k, Some(build_dir));
-}
-
-fn gen_evm_verifier<C: AppCircuitExt<Fr>>(config_path: &Path, vkey_path: &Path, path_out: &Path) {
-    let config: FlexGateConfigParams =
-        serde_json::from_slice(&fs::read(config_path).unwrap()).unwrap();
-
-
-    let params = read_params(config.k as u32);
-
-    let circuit = C::default();
-
-    let num_instance = circuit.num_instance();
-
-    let vk = read_vkey::<C>(vkey_path).expect("Failed to read vkey");
-
-    let _ = gen_evm_verifier_shplonk::<C>(&params, &vk, num_instance, Some(path_out));
-}
-
-fn gen_evm_proof<C: AppCircuitExt<Fr>>(
-    config_path: &Path,
-    build_dir: &Path,
-    path_in: &Path,
-    path_out: Option<&Path>,
-    instances: Option<&mut Vec<Vec<Fr>>>,
-) -> Vec<u8> {
-    let config: FlexGateConfigParams =
-        serde_json::from_slice(&fs::read(config_path).unwrap()).unwrap();
-
-    let (params, pk) = C::setup(&config, Some(build_dir));
-
-    let state: SyncState = serde_json::from_slice(&fs::read(path_in).unwrap()).unwrap();
-
-    let builder = RefCell::from(GateThreadBuilder::new(false));
-
-    let circuit = C::new_from_state(builder, &state);
-
-    let public_inputs = circuit.instances();
-
-    set_config(&config);
-
-    let proof = gen_evm_proof_shplonk(&params, &pk, circuit, public_inputs.clone());
-
-    if let Some(path) = path_out {
-        fs::write(path, proof.clone()).expect("Failed to write proof to file");
-    }
-
-    if let Some(instances) = instances {
-        *instances = public_inputs;
-    }
-
-    proof
-}
-
-fn gen_evm_calldata<C: AppCircuitExt<Fr>>(
-    config_path: &Path,
-    build_dir: &Path,
-    path_in: &Path,
-    path_out: &Path,
-) {
-    let mut instances = vec![];
-    let proof = gen_evm_proof::<C>(config_path, build_dir, path_in, None, Some(&mut instances));
-
-    write_calldata(&instances, &proof, path_out).unwrap();
-}
-
-fn set_config(config: &FlexGateConfigParams) {
-    set_var("LOOKUP_BITS", (config.k - 1).to_string());
-    set_var(
-        "FLEX_GATE_CONFIG_PARAMS",
-        serde_json::to_string(&config).unwrap(),
-    );
 }
