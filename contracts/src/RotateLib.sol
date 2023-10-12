a9ed3a650d51b8fe94c6313413ed3ad9f3acb317
Willem Olding <willemolding@gmail.com>
2023-10-12 12:32:50 +1100

    add draft of encoding. Something weird is going on though

--- contracts/src/RotateLib.sol
+++ contracts/src/RotateLib.sol
@@ -14,8 +14,23 @@ library RotateLib {
     * @param args The arguments for the sync step
     * @return The public input commitment that can be sent to the verifier contract.
      */
-    function toInputCommitment(RotateInput memory args, bytes32 finalizedHeaderRoot) internal pure returns (uint256[] memory) {
-        // TODO: Impment this
-        return new uint256[](0);
+    function toInputCommitment(RotateInput memory args, bytes32 finalizedHeaderRoot) internal pure returns (uint256[65] memory) {
+        uint256[65] memory inputs;
+
+        inputs[0] = uint256(args.syncCommitteePoseidon);
+
+        // uint256 syncCommitteeSSZNumeric = uint256(args.syncCommitteeSSZ);
+        // for (uint256 i = 0; i < 32; i++) {
+        //     inputs[32 - 1 - i] = syncCommitteeSSZNumeric % 2 ** 8;
+        //     syncCommitteeSSZNumeric = syncCommitteeSSZNumeric / 2 ** 8;
+        // }
+
+        // uint256 finalizedHeaderRootNumeric = uint256(finalizedHeaderRoot);
+        // for (uint256 j = 0; j < 32; j++) {
+        //     inputs[64 - j] = finalizedHeaderRootNumeric % 2 ** 8;
+        //     finalizedHeaderRootNumeric = finalizedHeaderRootNumeric / 2 ** 8;
+        // }
+
+        return inputs;
     }
 }
--- contracts/test/RotateExternal.sol
+++ contracts/test/RotateExternal.sol
@@ -12,6 +12,12 @@ contract RotateExternal {
     using RotateLib for RotateLib.RotateInput;
 
     function toInputCommitment(RotateLib.RotateInput calldata args, bytes32 finalizedHeaderRoot) public pure returns (uint256[] memory) {
-        return args.toInputCommitment(finalizedHeaderRoot);
+        uint256[65] memory commitment = args.toInputCommitment(finalizedHeaderRoot);
+        // copy all elements into a dynamic array. We need to do this because ethers-rs has a bug that can't support uint256[65] return types
+        uint256[] memory result = new uint256[](65);
+        // for (uint256 i = 0; i < commitment.length; i++) {
+        //     result[i] = commitment[i];
+        // }
+        return result;
     }
 }
