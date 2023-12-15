99442808df44823d6403a3cd255e8518526baf91
Timofey Luin <timofey.luin@gmail.com>
2023-12-15 15:58:27 +0100

    document CLI

--- prover/src/args.rs
+++ prover/src/args.rs
@@ -9,7 +9,11 @@ use strum::EnumString;
 
 #[derive(Clone, clap::Parser)]
 #[command(name = "spectre-prover")]
-#[command(about = "Spectre prover", long_about = None)]
+#[command(
+    about = "Spectre prover",
+    long_about = "Spectre is a Zero-Knowledge (ZK) coprocessor designed to offload intensive verification of block headers via Altair lightclient protocol.
+"
+)]
 pub struct Cli {
     #[command(subcommand)]
     pub subcommand: BaseCmd,
@@ -20,6 +24,7 @@ pub struct Cli {
 
 #[derive(Clone, clap::Args)]
 pub struct BaseArgs {
+    /// Path to config directory
     #[clap(long, short, default_value = "./lightclient-circuits/config")]
     pub config_dir: PathBuf,
 }
@@ -27,17 +32,22 @@ pub struct BaseArgs {
 #[derive(Clone, clap::Parser)]
 #[allow(clippy::large_enum_variant)]
 pub enum BaseCmd {
+    /// Deploy prover RPC server.
     Rpc {
+        /// Port for RPC server to listen on
         #[clap(long, short, default_value = "3000")]
         port: String,
     },
+    /// Circuit related commands.
     Circuit {
         #[command(subcommand)]
         proof: ProofCmd,
 
+        /// Network spec
         #[clap(long, short, default_value = "mainnet")]
         spec: Spec,
     },
+    /// Misc utility commands.
     Utils {
         #[command(subcommand)]
         method: UtilsCmd,
@@ -46,51 +56,66 @@ pub enum BaseCmd {
 
 #[derive(Clone, clap::Subcommand)]
 pub enum ProofCmd {
+    /// Step circuit - verifies Beacon chain block header and the execution payload.
     SyncStep {
         #[command(subcommand)]
         operation: OperationCmd,
 
+        /// Circuit degree
         #[clap(long, short, default_value = "22")]
         k: u32,
 
+        /// Path to prover key
         #[clap(long, short)]
         pk_path: PathBuf,
     },
+    /// Step circuit (compressed) - verifies Beacon chain block header and the execution payload. Uses aggregation to reduce verifier cost.
     SyncStepCompressed {
         #[command(subcommand)]
         operation: OperationCmd,
 
+        /// Circuit degree (first stage)
         #[clap(long, short, default_value = "20")]
         k: u32,
 
+        /// Path to prover key (first stage)
         #[clap(long, short)]
         pk_path: PathBuf,
 
-        #[clap(short='K', long, default_value = "23")]
+        /// Circuit degree (compression stage)
+        #[clap(short = 'K', long, default_value = "23")]
         verifier_k: u32,
 
-        #[clap(short='P', long)]
+        /// Path to prover key (compression stage)
+        #[clap(short = 'P', long)]
         verifier_pk_path: PathBuf,
 
+        /// Number of lookup bits (compression stage)
         #[clap(short = 'L', long)]
         verifier_lookup_bits: Option<usize>,
     },
+    /// Committee update circuit (compressed) - maps next sync committee root to the Poseidon commitment. Uses aggregation to reduce verifier cost.
     CommitteeUpdate {
         #[command(subcommand)]
         operation: OperationCmd,
 
+        /// Circuit degree (first stage)
         #[clap(long, short, default_value = "20")]
         k: u32,
 
+        /// Path to prover key (first stage)
         #[clap(long, short)]
         pk_path: PathBuf,
 
-        #[clap(short='K', long, default_value = "24")]
+        /// Circuit degree (compression stage)
+        #[clap(short = 'K', long, default_value = "24")]
         verifier_k: u32,
 
-        #[clap(short='P', long)]
+        /// Path to prover key (compression stage)
+        #[clap(short = 'P', long)]
         verifier_pk_path: PathBuf,
 
+        /// Number of lookup bits (compression stage)
         #[clap(short = 'L', long)]
         verifier_lookup_bits: Option<usize>,
     },
@@ -98,11 +123,15 @@ pub enum ProofCmd {
 
 #[derive(Clone, clap::Subcommand)]
 pub enum OperationCmd {
+    /// Generate prover and verifier keys
     Setup,
+    /// Generate Solidity verifier contract
     GenVerifier {
+        /// Path to generedated Solidity contract
         #[clap(long, short = 'o')]
         solidity_out: PathBuf,
 
+        /// Flag whether to estimate gas
         #[clap(long, short)]
         estimate_gas: bool,
     },
@@ -122,6 +151,7 @@ pub enum Spec {
 #[derive(Clone, clap::Subcommand)]
 pub enum UtilsCmd {
     CommitteePoseidon {
+        /// Beacon API URL
         #[clap(long, short)]
         beacon_api: String,
     },
